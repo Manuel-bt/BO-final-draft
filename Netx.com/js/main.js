79f89c7ef3d1735c0d9adad82e1be43a87be9717
Emmanuel-Thomas <emmanuelthomas@bodadrop.com>
2020-10-03 08:28:48 +0300

    Mofified Welcome, Home(added video pop up function)

--- Netx.com/js/main.js
+++ Netx.com/js/main.js
@@ -1,38 +1,91 @@
 $(document).ready(function(){
 
-    // Trending, High Rated Carousel
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:8,
-        nav:false,
-        responsive:{
-            0:{
-                items:4
-            },
-            600:{
-                items:5
-            },
-            1000:{
-                items:6
-            }
+// Trending, High Rated,etc Carousel
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:8,
+    nav:false,
+    responsive:{
+        0:{
+            items:4
+        },
+        600:{
+            items:5
+        },
+        1000:{
+            items:6
         }
-    });
+    }
+});
+//Carousel ENd 
+
+// isotope filter
+var $grid = $(".grid").isotope({
+    itemSelector : '.grid-item',
+    layoutMode : 'fitRows'
+});
+    
+// filter items on button click
+$(".button-group").on("click", "button", function(){
+    var filterValue = $(this).attr('data-filter');
+    $grid.isotope({ filter: filterValue});
+})
+//isotope filter ENd    
     
 
+
+//Welcome Page scroll effect
+    (function ($) {
+        "use strict"; // Start of use strict
+        // Smooth scrolling using jQuery easing
+        $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+            if (
+                location.pathname.replace(/^\//, "") ==
+                    this.pathname.replace(/^\//, "") &&
+                location.hostname == this.hostname
+            ) {
+                var target = $(this.hash);
+                target = target.length
+                    ? target
+                    : $("[name=" + this.hash.slice(1) + "]");
+                if (target.length) {
+                    $("html, body").animate(
+                        {
+                            scrollTop: target.offset().top - 70,
+                        },
+                        1000,
+                        "easeInOutExpo"
+                    );
+                    return false;
+                }
+            }
+        });
     
+        // Closes responsive menu when a scroll trigger link is clicked
+        $(".js-scroll-trigger").click(function () {
+            $(".navbar-collapse").collapse("hide");
+        });
     
-          
-    // isotope filter
-    var $grid = $(".grid").isotope({
-        itemSelector : '.grid-item',
-        layoutMode : 'fitRows'
-    });
+        // Activate scrollspy to add active class to navbar items on scroll
+        $("body").scrollspy({
+            target: "#mainNav",
+            offset: 100,
+        });
     
-    // filter items on button click
-    $(".button-group").on("click", "button", function(){
-        var filterValue = $(this).attr('data-filter');
-        $grid.isotope({ filter: filterValue});
-    })
-    
-    
-    });
+        // Collapse Navbar
+        var navbarCollapse = function () {
+            if ($("#mainNav").offset().top > 100) {
+                $("#mainNav").addClass("navbar-shrink");
+            } else {
+                $("#mainNav").removeClass("navbar-shrink");
+            }
+        };
+        // Collapse now if page is not at top
+        navbarCollapse();
+        // Collapse the navbar when page is scrolled
+        $(window).scroll(navbarCollapse);
+
+    })(jQuery); // End of use strict
+    //ENd of welcome scroll effect
+
+});
